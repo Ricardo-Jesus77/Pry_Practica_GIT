a23f337eeb8dd6a3d90c60cb47a3fd3083bfb5ec
Ricardo-Jesus77 <20250990@uthh.edu.mx>
2026-06-27 14:31:49 -0600

    cambios

---
+++ Boleto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pry_Practica_GIT
+{
+    public abstract class Boleto
+    {
+        public double PrecioBase { get; set; }
+
+        public Boleto(double precioBase)
+        {
+            PrecioBase = precioBase;
+        }
+
+        public abstract double CalcularPrecioFinal();
+    }
+}
