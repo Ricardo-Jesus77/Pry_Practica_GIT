2ab16e0eea7aabc3dcfd74cf8a380e24446a0127
Daniel Medina <danielmediv@gmail.com>
2026-06-28 20:51:18 -0600

    Se agrega la subclase BoletoAdultoMayor con sus atributos

---
+++ BoletoAdultoMayor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pry_Practica_GIT
+{
+    public class BoletoAdultoMayor : Boleto
+    { 
+        public string NumInapam {  get; set; }
+
+        public BoletoAdultoMayor(double precioBase,string credencial) : base(precioBase)
+        {
+            NumInapam = credencial;
+        }
+       
+
+        public override double CalcularPrecioFinal()
+        {
+            return PrecioBase * 0.50;
+        }
+
+    }
+}
